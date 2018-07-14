cd5289276377769f22f9378685e73d8d2e152af3
chnainahanen <40662736+chnainahanen@users.noreply.github.com>
2018-07-14 15:17:05 +0200

    add contrat controller

---
+++ src/main/java/com/consulting/core/cra/controller/ContratController.java
@@ -0,0 +1,67 @@
+package com.consulting.core.cra.controller;
+
+import com.consulting.core.cra.model.Company;
+import com.consulting.core.cra.model.Contract;
+import com.consulting.core.cra.repository.ContratRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Created by hanen on 14/07/2018.
+ */
+
+@RestController
+@RequestMapping("/api/")
+public class ContratController {
+
+    @Autowired
+    private ContratRepository repository;
+
+
+    @RequestMapping(value = "contracts", method = RequestMethod.GET)
+    public Collection<Contract> getContracts() {
+        return repository.findAll().stream()
+                .collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "contract", method = RequestMethod.POST)
+    public Contract addContat(@Valid @RequestBody Contract contract) {
+        return repository.save(company);
+    }
+
+    @RequestMapping("clients/{id}")
+    public Contract getContractById(@PathVariable(value = "id") Long contractId) {
+        return repository.findById(contractId)
+                .orElseThrow(() -> new ResourceNotFoundException("companyId"));
+    }
+
+    @RequestMapping(value = "client/{id}", method = RequestMethod.DELETE)
+    public void deleteContract(@PathVariable("id") long id) {
+        Optional<Contract> currentContract = null;
+        currentContract = repository.findById(id);
+        if (currentContract !=null ) {
+            repository.deleteById(id);
+        }
+    }
+
+    @RequestMapping(value = "contract/{id}", method = RequestMethod.PUT)
+    public Contract updateContract(@PathVariable(value = "id") String contratId,
+                              @Valid @RequestBody Contract contractDetails) {
+
+        Contract contrat = repository.findById(Long.parseLong(contratId))
+                .orElseThrow(() -> new ResourceNotFoundException("contratId"));
+        System.out.println(contrat);
+        contrat.setContratId(contractDetails.getContractId());
+          Contract updatedContract = repository.save(contrat);
+        System.out.println(updatedContract);
+
+        return updatedContract;
+    }
+
+}
